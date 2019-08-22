eceb64373d342b064f235332ed00260ebf92c5b1
ChinKX <chinkx1205@gmail.com>
2019-08-22 23:42:25 +0800

    Circle CI_v2

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:tenant_management_app/app_screens/home.dart';
 import 'package:flutter/services.dart';
 
 void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "TenantCare",
-    home: MyApp(),
-  )
+  MyApp(),
 );
 
 class MyApp extends StatelessWidget {
@@ -17,6 +13,10 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return Home();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "TenantCare",
+      home: Home(),
+    );
   }
 }
